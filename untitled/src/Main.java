02fd0427293950e7293764804b9433da7e55b06b
jinjy <1322421385@qq.com>
2025-03-26 19:25:27 +0800

    第一次提交数据

---
+++ untitled/src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: jinxy
+ * @Date: ${DATE} - ${MONTH} - ${DAY} - ${TIME}
+ * @Description:
+ * @version: 1.0
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ untitled/src/Student.java
@@ -0,0 +1,8 @@
+/**
+ * @Auther: jinxy
+ * @Date: 2025/3/26 - 03 - 26 - 19:19
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Student {
+}
