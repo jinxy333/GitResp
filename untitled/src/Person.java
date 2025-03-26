cdf816968e678ba23a27aa15ff8c640e8dab65ab
jinjy <1322421385@qq.com>
2025-03-26 20:02:11 +0800

    第三次提交数据

---
+++ untitled/src/Person.java
@@ -0,0 +1,12 @@
+/**
+ * @Auther: jinxy
+ * @Date: 2025/3/26 - 03 - 26 - 20:01
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Person {
+    private int age ; //
+    private int stu ; //stu
+
+
+}
